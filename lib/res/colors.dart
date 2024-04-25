fd2b210d19958fa2a48c17d3b268e82cab6643a7
SaadAhmed <99617062+SaadAhmed012@users.noreply.github.com>
2024-04-26 03:39:59 +0500

    Added AppColors

---
+++ lib/res/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const whiteColor = Colors.white;
+  static const blackColor = Colors.black;
+  static const blueColor = Colors.blue;
+}
